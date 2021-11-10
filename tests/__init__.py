f47b395398d8c8a9621d1019a52ec7066cb7b5c2
Daniel Hoherd <daniel.hoherd@gmail.com>
2021-11-10 10:17:22 -0800

    Drop 1.16, add 1.21. Load circleci config for k8s versions in pytest.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,11 +1,15 @@
 from pathlib import Path
 
 import git
+import yaml
 
 # The top-level path of this repository
 git_repo = git.Repo(__file__, search_parent_directories=True)
 git_root_dir = Path(git_repo.git.rev_parse("--show-toplevel"))
 
-# This should match the major.minor version list in .circleci/generate_circleci_config.py
-# Patch version should always be 0
-supported_k8s_versions = ["1.17.0", "1.18.0", "1.19.0", "1.20.0", "1.21.0"]
+with open(f"{git_root_dir}/.circleci/config.yml") as f:
+    circleci_config = yaml.safe_load(f.read())
+
+# fmt: off
+supported_k8s_versions = circleci_config["workflows"]["install-airflow-chart"]["jobs"][4]["airflow-test"]["matrix"]["parameters"]["kube_version"]
+# fmt: on
