ffc050fdeba71e6a005be88cd45708e9222a6b0f
pgvishnuram <vishnu@astronomer.io>
2022-03-10 15:05:35 +0530

    fix typo in scc test case

--- tests/chart_tests/test_scc.py
+++ tests/chart_tests/test_scc.py
@@ -10,7 +10,7 @@ class TestAirflowsccprivileges:
     def test_scc_privileges_defaults(self, kube_version):
         """Test sairflow cc privileges with defaults - disabled."""
         docs = render_chart(
-            kube_version="v4.1.0",
+            kube_version=kube_version,
             values={},
             show_only="templates/airflow-scc-anyuid.yaml",
         )
