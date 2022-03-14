006062311e7d1734ee26f3769d7a0ca3347db9c8
Daniel Hoherd <daniel.hoherd@gmail.com>
2022-03-14 10:25:13 -0700

    Fix typo

--- tests/chart_tests/test_scc.py
+++ tests/chart_tests/test_scc.py
@@ -8,7 +8,7 @@ from .. import supported_k8s_versions
 @pytest.mark.parametrize("kube_version", supported_k8s_versions)
 class TestAirflowSccPrivileges:
     def test_scc_privileges_defaults(self, kube_version):
-        """Test sairflow cc privileges with defaults - disabled."""
+        """Test airflow scc privileges with defaults - disabled."""
         docs = render_chart(
             kube_version=kube_version,
             values={},
