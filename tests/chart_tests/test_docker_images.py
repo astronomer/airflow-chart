cd342bba6f63f19c7874168ff29b60437c4dae1b
Mishal Shah <mishal.shah@astronomer.ai>
2022-04-04 12:04:50 +0530

    Updating test logic to not pull docker image for test and adding more checks docker daemon.

--- tests/chart_tests/test_docker_images.py
+++ tests/chart_tests/test_docker_images.py
@@ -2,6 +2,7 @@ import docker
 import jmespath
 import pytest
 
+from tests.chart_tests.conftest import docker_daemon_present
 from tests.chart_tests.helm_template_generator import render_chart
 
 
@@ -21,12 +22,12 @@ def list_docker_images():
     return docker_images
 
 
-# Pulling docker image test
 @pytest.mark.parametrize("docker_image", list_docker_images())
+@pytest.mark.skipif(not docker_daemon_present(), reason="Docker daemon not available")
+@pytest.mark.flaky(reruns=5, reruns_delay=1)
 def test_docker_image(docker_client, docker_image):
     docker_image = docker_image.replace('"', "").strip()
     try:
-        # Pulling docker image
-        docker_client.images.pull(docker_image)
+        docker_client.images.get_registry_data(docker_image)
     except docker.errors.APIError as exc:
-        assert False, f"'Unable to pull docker image: {docker_image} | Error: {exc}"
+        assert False, f"'Error reading image: {docker_image} | Error: {exc}"
