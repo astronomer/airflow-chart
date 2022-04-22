4b554911e63dc777cdb5fc395fa3cd7993604009
rishkarajgi <rishabh.karajgi@gmail.com>
2022-04-23 00:41:00 +0530

    added test case doc

---
+++ tests/chart_tests/test_logging_sidecar.py
@@ -0,0 +1,39 @@
+import pytest
+
+from tests.chart_tests.helm_template_generator import render_chart
+
+from .. import supported_k8s_versions
+
+
+@pytest.mark.parametrize("kube_version", supported_k8s_versions)
+class TestLoggingSidecar:
+    def test_logging_sidecar_config_defaults(self, kube_version):
+        """Test logging sidecar config with defaults"""
+        docs = render_chart(
+            kube_version=kube_version,
+            values={
+                "loggingSidecar": {"enabled": True},
+                "airflow": {
+                    "elasticsearch": {
+                        "enabled": True,
+                        "connection": {"user": "testuser", "pass": "testpass"},
+                    }
+                },
+            },
+            show_only="templates/logging-sidecar-configmap.yaml",
+        )
+        assert len(docs) == 1
+        doc = docs[0]
+        assert "ConfigMap" == doc["kind"]
+        assert "v1" == doc["apiVersion"]
+        assert "testuser" in doc["data"]["vector-config.yaml"]
+        assert "testpass" in doc["data"]["vector-config.yaml"]
+
+    def test_logging_sidecar_config_disabled(self, kube_version):
+        """Test logging sidecar config with flag disabled"""
+        docs = render_chart(
+            kube_version=kube_version,
+            values={"loggingSidecar": {"enabled": False}},
+            show_only="templates/logging-sidecar-configmap.yaml",
+        )
+        assert len(docs) == 0
