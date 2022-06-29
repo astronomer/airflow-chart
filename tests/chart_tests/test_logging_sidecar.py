5e8c4d2a2f89bb0cefe2b72f51b1cc89c250a8d1
rishkarajgi <rishabh.karajgi@gmail.com>
2022-06-29 19:44:01 +0530

    updated condition to load vector configmap

--- tests/chart_tests/test_logging_sidecar.py
+++ tests/chart_tests/test_logging_sidecar.py
@@ -53,31 +53,7 @@ class TestLoggingSidecar:
           enabled: true
           customConfig: true
           name: sidecar-logging-consumer
-          airflowSidecarConfig: |
-            log_schema:
-              timestamp_key : "@timestamp"
-            data_dir: "${SIDECAR_LOGS}"
-            sources:
-              generate_syslog:
-                type: file
-                include:
-                  - "${SIDECAR_LOGS}/*.log"
-                read_from: beginning
-            transforms:
-              transform_syslog:
-                type: add_fields
-                inputs:
-                  - generate_syslog
-                fields:
-                  component: "${COMPONENT:--}"
-                  workspace: "${WORKSPACE:--}"
-                  release: "${RELEASE:--}"
-            sinks:
-              out:
-                type: datadog
-                inputs:
-                  - transform_syslog
-                """
+            """
         )
         values = yaml.safe_load(test_custom_sidecar_config)
         docs = render_chart(
@@ -85,9 +61,4 @@ class TestLoggingSidecar:
             values=values,
             show_only="templates/logging-sidecar-configmap.yaml",
         )
-        assert len(docs) == 1
-        doc = docs[0]
-        assert "ConfigMap" == doc["kind"]
-        assert "v1" == doc["apiVersion"]
-        assert (vc := yaml.safe_load(doc["data"]["vector-config.yaml"]))
-        assert vc["sinks"]["out"]["type"] == "datadog"
+        assert len(docs) == 0
