41ba5aaaa37b2caa9dcbe52f4c95b325748f4be4
Daniel Hoherd <daniel.hoherd@gmail.com>
2022-11-10 18:03:52 -0500

    First crack at templatizing this

---
+++ .circleci/generate_circleci_config.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""This script is used to create the circle config file so that we can stay
+DRY."""
+import os
+from pathlib import Path
+
+from jinja2 import Template
+
+# When adding a new version, look up the most
+# recent patch version on Dockerhub
+# https://hub.docker.com/r/kindest/node/tags
+# This should match what is in tests/__init__.py
+kube_versions = ["1.21.14", "1.22.15", "1.23.13", "1.24.7"]
+executors = ["CeleryExecutor", "LocalExecutor", "KubernetesExecutor"]
+# https://circleci.com/docs/2.0/building-docker-images/#docker-version
+remote_docker_version = "20.10.18"
+ci_runner_version = "2022-11"
+
+
+def main():
+    """Render the Jinja2 template file."""
+    circle_directory = os.path.dirname(os.path.realpath(__file__))
+    config_template_path = os.path.join(circle_directory, "config.yml.j2")
+    config_path = os.path.join(circle_directory, "config.yml")
+
+    templated_file_content = Path(config_template_path).read_text()
+    template = Template(templated_file_content)
+    config = template.render(
+        kube_versions=kube_versions,
+        executors=executors,
+        ci_runner_version=ci_runner_version,
+        remote_docker_version=remote_docker_version,
+    )
+    with open(config_path, "w") as circle_ci_config_file:
+        warning_header = (
+            "# Warning: automatically generated file\n"
+            + "# Please edit config.yml.j2, and use the script generate_circleci_config.py\n"
+        )
+        circle_ci_config_file.write(warning_header)
+        circle_ci_config_file.write(config)
+        circle_ci_config_file.write("\n")
+
+
+if __name__ == "__main__":
+    main()
