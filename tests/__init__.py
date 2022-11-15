6d0f78bb13e5e8e3a1b04a1e4c90b1cbfd17d685
Daniel Hoherd <danielh@astronomer.io>
2022-11-15 09:26:13 -0500

    5180 only test some k8s versions by default (#369)

--- tests/__init__.py
+++ tests/__init__.py
@@ -10,6 +10,6 @@ git_root_dir = Path(git_repo.git.rev_parse("--show-toplevel"))
 with open(f"{git_root_dir}/.circleci/config.yml") as f:
     circleci_config = yaml.safe_load(f.read())
 
-# fmt: off
-supported_k8s_versions = circleci_config["workflows"]["install-airflow-chart"]["jobs"][4]["airflow-test"]["matrix"]["parameters"]["kube_version"]
-# fmt: on
+# This should match the major.minor version list in .circleci/generate_circleci_config.py
+# Patch version should always be 0
+supported_k8s_versions = ["1.21.0", "1.22.0", "1.23.0", "1.24.0"]
