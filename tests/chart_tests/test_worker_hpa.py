4c41515920ca6eb71ee5b305fe66d46c3fc814a9
Manu Sharma <manu@astronomer.io>
2023-01-26 13:08:46 -0500

    Update hpa test

--- tests/chart_tests/test_worker_hpa.py
+++ tests/chart_tests/test_worker_hpa.py
@@ -29,3 +29,39 @@ class TestWorkerHpa:
         assert len(docs) == 1
         doc = docs[0]
         assert "HorizontalPodAutoscaler" == doc["kind"]
+        _, minor, _ = (int(x) for x in kube_version.split("."))
+        if minor >= 25:
+            assert "autoscaling/v2" == doc["apiVersion"]
+        else:
+            assert "autoscaling/v2beta1" == doc["apiVersion"]
+
+    def test_worker_hpa_metrics(self, kube_version):
+        docs = render_chart(
+            kube_version=kube_version,
+            values={
+                "airflow": {"executor": "CeleryExecutor"},
+                "workers": {"autoscaling": {"enabled": True}},
+            },
+            show_only="templates/workers/worker-horizontalpodautoscaler.yaml",
+        )
+        assert len(docs) == 1
+        doc = docs[0]
+        assert doc["spec"]["metrics"][0]["resource"]["name"] == "cpu"
+        assert doc["spec"]["metrics"][1]["resource"]["name"] == "memory"
+        _, minor, _ = (int(x) for x in kube_version.split("."))
+        if minor >= 25:
+            assert (
+                doc["spec"]["metrics"][0]["resource"]["target"]["averageUtilization"]
+                == 80
+            )
+            assert (
+                doc["spec"]["metrics"][1]["resource"]["target"]["averageUtilization"]
+                == 80
+            )
+        else:
+            assert (
+                doc["spec"]["metrics"][0]["resource"]["targetAverageUtilization"] == 80
+            )
+            assert (
+                doc["spec"]["metrics"][1]["resource"]["targetAverageUtilization"] == 80
+            )
