a89931040624fadd26ba944b49b4a21df810cabd
Daniel Hoherd <daniel.hoherd@gmail.com>
2023-03-16 18:34:52 -0400

    Override new airflow statsd cm with Astronomer's statsd config

---
+++ tests/chart_tests/test_statsd.py
@@ -0,0 +1,28 @@
+import pytest
+import yaml
+
+from tests.chart_tests.helm_template_generator import render_chart
+
+from .. import supported_k8s_versions
+
+
+@pytest.mark.parametrize("kube_version", supported_k8s_versions)
+class TestStatsdConfigMap:
+    def test_statsd_configmap_defaults(self, kube_version):
+        """Test that default statsd is good"""
+        docs = render_chart(
+            kube_version=kube_version,
+            show_only="charts/airflow/templates/configmaps/statsd-configmap.yaml",
+        )
+        assert len(docs) == 1
+        doc = docs[0]
+        assert doc["kind"] == "ConfigMap"
+        assert doc["apiVersion"] == "v1"
+        yml = yaml.safe_load(doc["data"]["mappings.yml"])
+        assert len(yml["mappings"]) == 19
+        assert yml["mappings"][-1] == {
+            "action": "drop",
+            "match": ".",
+            "match_type": "regex",
+            "name": "dropped",
+        }
