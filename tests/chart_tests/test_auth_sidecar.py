b32a58442ceec18008884a87b6d8cf0976ec9f60
pgvishnuram <81585115+pgvishnuram@users.noreply.github.com>
2024-02-01 21:32:10 +0530

    Update tests/chart_tests/test_auth_sidecar.py
    Co-authored-by: Daniel Hoherd <danielh@astronomer.io>

--- tests/chart_tests/test_auth_sidecar.py
+++ tests/chart_tests/test_auth_sidecar.py
@@ -8,17 +8,15 @@ from . import get_containers_by_name
 
 @pytest.mark.parametrize("kube_version", supported_k8s_versions)
 class TestAuthSidecar:
+    show_only = [
+        "templates/dag-deploy/dag-server-auth-sidecar-configmap.yaml",
+        "templates/flower/flower-auth-sidecar-configmap.yaml",
+        "templates/webserver/webserver-auth-sidecar-configmap.yaml",
+    ]
+
     def test_auth_sidecar_config_defaults(self, kube_version):
         """Test logging sidecar config with defaults"""
-        docs = render_chart(
-            kube_version=kube_version,
-            values={},
-            show_only=[
-                "templates/dag-deploy/dag-server-auth-sidecar-configmap.yaml",
-                "templates/flower/flower-auth-sidecar-configmap.yaml",
-                "templates/webserver/webserver-auth-sidecar-configmap.yaml",
-            ],
-        )
+        docs = render_chart(kube_version=kube_version, values={}, show_only=self.show_only)
         assert len(docs) == 0
 
     def test_auth_sidecar_config_enabled_with_celeryexecutor(self, kube_version):
