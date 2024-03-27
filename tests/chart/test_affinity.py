3e64bab3dcacc2ac01b6234d4b5b8e488b1742eb
Daniel Hoherd <daniel.hoherd@gmail.com>
2024-03-27 16:48:05 -0400

    Add a breaking test that shows the problem.

--- tests/chart/test_affinity.py
+++ tests/chart/test_affinity.py
@@ -20,7 +20,7 @@ class TestAffinity:
         "charts/airflow/templates/workers/worker-deployment.yaml",
     ]
 
-    def test_scheduler_global_affinity(self, kube_version):
+    def test_scheduler_global_affinity_defaults(self, kube_version):
         """Test that airflow chart affinity is set correctly when using our default values."""
 
         docs = render_chart(
@@ -94,3 +94,100 @@ class TestAffinity:
                 }
             },
         ]
+
+    def test_scheduler_global_affinity_override_globals(self, kube_version):
+        """Test that airflow chart affinity is set correctly when overriding the global affinity."""
+
+        global_affinity = {
+            "podAntiAffinity": {
+                "preferredDuringSchedulingIgnoredDuringExecution": [
+                    {
+                        "podAffinityTerm": {
+                            "labelSelector": {
+                                "matchLabels": {
+                                    "THIS_IS_THE_GLOBAL_LABEL_KEY": "THIS_IS_THE_GLOBAL_LABEL_VALUE"
+                                }
+                            },
+                            "topologyKey": "SOME_TOPOLOGY_KEY",
+                        },
+                        "weight": 999,
+                    }
+                ]
+            }
+        }
+
+        docs = render_chart(
+            kube_version=kube_version,
+            values={
+                "airflow": {
+                    "affinity": global_affinity,
+                    "flower": {"enabled": True},
+                    "pgbouncer": {"enabled": True},
+                    "executor": "CeleryExecutor",
+                }
+            },
+            show_only=self.affinity_files,
+        )
+        assert len(docs) == 7
+        assert [
+            doc.get("spec").get("template").get("spec").get("affinity") for doc in docs
+        ] == [
+            {},
+            {},
+            {},
+            {
+                "podAntiAffinity": {
+                    "preferredDuringSchedulingIgnoredDuringExecution": [
+                        {
+                            "podAffinityTerm": {
+                                "labelSelector": {
+                                    "matchExpressions": [
+                                        {
+                                            "key": "component",
+                                            "operator": "In",
+                                            "values": ["scheduler"],
+                                        }
+                                    ]
+                                },
+                                "topologyKey": "kubernetes.io/hostname",
+                            },
+                            "weight": 100,
+                        }
+                    ]
+                }
+            },
+            {},
+            {
+                "podAntiAffinity": {
+                    "preferredDuringSchedulingIgnoredDuringExecution": [
+                        {
+                            "podAffinityTerm": {
+                                "labelSelector": {
+                                    "matchLabels": {"component": "webserver"}
+                                },
+                                "topologyKey": "kubernetes.io/hostname",
+                            },
+                            "weight": 100,
+                        }
+                    ]
+                }
+            },
+            {
+                "podAntiAffinity": {
+                    "preferredDuringSchedulingIgnoredDuringExecution": [
+                        {
+                            "podAffinityTerm": {
+                                "labelSelector": {
+                                    "matchLabels": {"component": "worker"}
+                                },
+                                "topologyKey": "kubernetes.io/hostname",
+                            },
+                            "weight": 100,
+                        }
+                    ]
+                }
+            },
+        ]
+
+        ### This currently breaks, showing the complaint of the customer. Scheduler has its own component-value-specified affinity and does not respect the global affinity.
+        ### https://github.com/astronomer/issues/issues/6214#issuecomment-2023955941
