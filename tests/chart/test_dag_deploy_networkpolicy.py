6694408c05a2ca60607a578b63650eace2bdd0c4
Daniel Hoherd <daniel.hoherd@gmail.com>
2024-07-11 15:57:39 -0400

    Add tests for networkPolicy that was confirmed working.

--- tests/chart/test_dag_deploy_networkpolicy.py
+++ tests/chart/test_dag_deploy_networkpolicy.py
@@ -18,7 +18,10 @@ class TestDagDeployNetworkPolicy:
     def test_dag_deploy_networkpolicy_dag_deploy_enabled(self, kube_version):
         """Test that a valid networkPolicy are rendered when dag-deploy is enabled."""
 
-        values = {"dagDeploy": {"enabled": True}}
+        values = {
+            "dagDeploy": {"enabled": True},
+            "platform": {"namespace": "test-ns-99", "release": "test-release-42"},
+        }
 
         docs = render_chart(
             kube_version=kube_version,
@@ -26,3 +29,37 @@ class TestDagDeployNetworkPolicy:
             values=values,
         )
         assert len(docs) == 1
+        spec = docs[0]["spec"]
+
+        assert list(spec["podSelector"].keys()) == ["matchLabels"]
+        assert spec["policyTypes"] == ["Ingress"]
+        assert spec["podSelector"]["matchLabels"] == {
+            "tier": "airflow",
+            "component": "dag-server",
+            "release": "release-name",
+        }
+
+        assert all(
+            [x["ports"] == [{"protocol": "TCP", "port": 8000}] for x in spec["ingress"]]
+        )
+
+        assert spec["ingress"][0]["from"] == [
+            {
+                "podSelector": {
+                    "matchLabels": {"release": "release-name", "tier": "airflow"}
+                }
+            }
+        ]
+
+        assert len(spec["ingress"][1]["from"]) == 1
+        assert (
+            spec["ingress"][1]["from"][0]["namespaceSelector"]["matchLabels"][
+                "kubernetes.io/metadata.name"
+            ]
+            == "test-ns-99"
+        )
+        assert spec["ingress"][1]["from"][0]["podSelector"]["matchLabels"] == {
+            "app": "houston",
+            "component": "houston",
+            "release": "test-release-42",
+        }
