4df744b613f209594a7801798c4172ad5df7b0ec
pgvishnuram <vishnu@astronomer.io>
2024-09-20 13:44:22 +0530

    add missing pod template test cases

--- tests/chart/test_pod_template.py
+++ tests/chart/test_pod_template.py
@@ -5,6 +5,13 @@ from tests.chart.helm_template_generator import render_chart
 from .. import supported_k8s_versions
 
 
+def common_pod_template_test(docs):
+    """Test common asserts for pod template pod spec."""
+    assert len(docs) == 1
+    doc = docs[0]
+    assert doc["kind"] == "ConfigMap"
+
+
 @pytest.mark.parametrize("kube_version", supported_k8s_versions)
 class TestPodTemplate:
     def test_pod_template_defaults(self, kube_version):
@@ -14,9 +21,8 @@ class TestPodTemplate:
             values={},
             show_only="charts/airflow/templates/configmaps/configmap.yaml",
         )
-        assert len(docs) == 1
+        common_pod_template_test(docs)
         doc = docs[0]
-        assert doc["kind"] == "ConfigMap"
         podTemplate = yaml.safe_load(doc["data"]["pod_template_file.yaml"])
         assert {"tier": "airflow", "component": "worker", "release": "release-name"} == podTemplate["metadata"]["labels"]
 
@@ -32,10 +38,42 @@ class TestPodTemplate:
             },
             show_only="charts/airflow/templates/configmaps/configmap.yaml",
         )
-        assert len(docs) == 1
+        common_pod_template_test(docs)
         doc = docs[0]
-        assert doc["kind"] == "ConfigMap"
         podTemplate = yaml.safe_load(doc["data"]["pod_template_file.yaml"])
-        assert {"tier": "airflow", "component": "worker", "release": "release-name", "service": "airflow", "servicetype": "workers"} == podTemplate[
-            "metadata"
-        ]["labels"]
+        assert {
+            "tier": "airflow",
+            "component": "worker",
+            "release": "release-name",
+            "service": "airflow",
+            "servicetype": "workers",
+        } == podTemplate["metadata"]["labels"]
+
+    def test_pod_template_pod_annotation_defaults(self, kube_version):
+        """Test airflow pod template labels overrides."""
+        docs = render_chart(
+            kube_version=kube_version,
+            values={},
+            show_only="charts/airflow/templates/configmaps/configmap.yaml",
+        )
+        common_pod_template_test(docs)
+        doc = docs[0]
+        podTemplate = yaml.safe_load(doc["data"]["pod_template_file.yaml"])
+        assert "annotations" not in podTemplate["metadata"]
+
+    def test_pod_template_pod_annotation_overrides(self, kube_version):
+        """Test airflow pod template labels overrides."""
+        docs = render_chart(
+            kube_version=kube_version,
+            values={
+                "airflow": {
+                    "airflowPodAnnotations": {"app.kubernetes.io": "airflow"},
+                    "workers": {"podAnnotations": {"app.worker.kubernetes.io": "workers"}},
+                }
+            },
+            show_only="charts/airflow/templates/configmaps/configmap.yaml",
+        )
+        common_pod_template_test(docs)
+        doc = docs[0]
+        podTemplate = yaml.safe_load(doc["data"]["pod_template_file.yaml"])
+        assert {"app.kubernetes.io": "airflow", "app.worker.kubernetes.io": "workers"} == podTemplate["metadata"]["annotations"]
