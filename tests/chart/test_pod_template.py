253891e4191267405b1a20eee44c8ec682f24087
pgvishnuram <vishnu@astronomer.io>
2024-09-20 14:00:53 +0530

    add missing pod template features

--- tests/chart/test_pod_template.py
+++ tests/chart/test_pod_template.py
@@ -25,6 +25,8 @@ class TestPodTemplate:
         doc = docs[0]
         podTemplate = yaml.safe_load(doc["data"]["pod_template_file.yaml"])
         assert {"tier": "airflow", "component": "worker", "release": "release-name"} == podTemplate["metadata"]["labels"]
+        assert "runtimeClassName" not in podTemplate["spec"]
+        assert "priorityClassName" not in podTemplate["spec"]
 
     def test_pod_template_labels_overrides(self, kube_version):
         """Test airflow pod template labels overrides."""
@@ -77,3 +79,35 @@ class TestPodTemplate:
         doc = docs[0]
         podTemplate = yaml.safe_load(doc["data"]["pod_template_file.yaml"])
         assert {"app.kubernetes.io": "airflow", "app.worker.kubernetes.io": "workers"} == podTemplate["metadata"]["annotations"]
+
+    def test_pod_template_pod_priority_class_name_overrides(self, kube_version):
+        """Test airflow pod template labels overrides."""
+        docs = render_chart(
+            kube_version=kube_version,
+            values={
+                "airflow": {
+                    "workers": {"priorityClassName": "criticalworkload"},
+                }
+            },
+            show_only="charts/airflow/templates/configmaps/configmap.yaml",
+        )
+        common_pod_template_test(docs)
+        doc = docs[0]
+        podTemplate = yaml.safe_load(doc["data"]["pod_template_file.yaml"])
+        assert "criticalworkload" == podTemplate["spec"]["priorityClassName"]
+
+    def test_pod_template_pod_runtime_class_name_overrides(self, kube_version):
+        """Test airflow pod template labels overrides."""
+        docs = render_chart(
+            kube_version=kube_version,
+            values={
+                "airflow": {
+                    "workers": {"runtimeClassName": "criticalworkload"},
+                }
+            },
+            show_only="charts/airflow/templates/configmaps/configmap.yaml",
+        )
+        common_pod_template_test(docs)
+        doc = docs[0]
+        podTemplate = yaml.safe_load(doc["data"]["pod_template_file.yaml"])
+        assert "criticalworkload" == podTemplate["spec"]["runtimeClassName"]
