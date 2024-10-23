c8fa7ee412c16514eac8a6052c9df64669eb94c1
pgvishnuram <vishnu@astronomer.io>
2024-10-23 12:50:54 +0530

    fix test cases

--- tests/chart/test_ingress.py
+++ tests/chart/test_ingress.py
@@ -22,7 +22,7 @@ class TestIngress:
         assert doc["spec"]["tls"][0]["secretName"] is None
 
     def test_airflow_ingress_tls_secret_overrides(self, kube_version):
-        """Test airflow ingress with defaults - KubernetesExecutor."""
+        """Test airflow ingress and tls secret overrides with KubernetesExecutor."""
         docs = render_chart(
             kube_version=kube_version,
             show_only="templates/ingress.yaml",
@@ -50,12 +50,36 @@ class TestIngress:
         doc = docs[0]
         assert "Ingress" == doc["kind"]
         assert "networking.k8s.io/v1" == doc["apiVersion"]
-        assert "/release-name/airflow" == docs[0]["spec"]["rules"][0]["http"]["paths"][0]["path"]
+        assert "/release-name/airflow" == doc["spec"]["rules"][0]["http"]["paths"][0]["path"]
 
         doc = docs[1]
         assert "Ingress" == doc["kind"]
         assert "networking.k8s.io/v1" == doc["apiVersion"]
-        assert "/release-name/airflow" == docs[0]["spec"]["rules"][0]["http"]["paths"][0]["path"]
+        assert "/release-name/flower(/|$)(.*)" == doc["spec"]["rules"][0]["http"]["paths"][0]["path"]
+
+    def test_airflow_ingress_with_celery_executor_with_tls_overides(self, kube_version):
+        """Test airflow ingress and tls secret overrides with CeleryExecutor."""
+        docs = render_chart(
+            kube_version=kube_version,
+            show_only="templates/ingress.yaml",
+            values={
+                "airflow": {"executor": "CeleryExecutor"},
+                "ingress": {"enabled": True, "baseDomain": "example.com", "tlsSecretName": "astronomer-tls"},
+            },
+        )
+        assert len(docs) == 2
+
+        doc = docs[0]
+        assert "Ingress" == doc["kind"]
+        assert "networking.k8s.io/v1" == doc["apiVersion"]
+        assert "/release-name/airflow" == doc["spec"]["rules"][0]["http"]["paths"][0]["path"]
+        assert "astronomer-tls" == doc["spec"]["tls"][0]["secretName"]
+
+        doc = docs[1]
+        assert "Ingress" == doc["kind"]
+        assert "networking.k8s.io/v1" == doc["apiVersion"]
+        assert "/release-name/flower(/|$)(.*)" == doc["spec"]["rules"][0]["http"]["paths"][0]["path"]
+        assert "astronomer-tls" == doc["spec"]["tls"][0]["secretName"]
 
     def test_airflow_ingress_with_dag_server(self, kube_version):
         """Test airflow ingress with DagServer."""
@@ -73,9 +97,10 @@ class TestIngress:
         rule_0 = docs[0]["spec"]["rules"][0]
         assert rule_0["http"]["paths"][0]["path"] == "/release-name/dags/(upload|downloads|healthz)(/.*)?"
         assert rule_0["host"] == "deployments.example.com"
+        assert docs[0]["spec"]["tls"][0]["secretName"] is None
 
-    def test_airflow_ingress_with_dag_server_ingress_annotation(self, kube_version):
-        """Test airflow ingress with DagServer."""
+    def test_airflow_ingress_with_dag_server_ingress_annotation_and_tls_secret(self, kube_version):
+        """Test airflow ingress annotation and tls secret with DagServer."""
 
         ingressAnnotation = {"kubernetes.io/ingress.class": "default"}
         docs = render_chart(
@@ -86,6 +111,7 @@ class TestIngress:
                     "enabled": True,
                     "baseDomain": "example.com",
                     "dagServerAnnotations": ingressAnnotation,
+                    "tlsSecretName": "astronomer-tls",
                 },
                 "dagDeploy": {"enabled": True},
             },
@@ -99,3 +125,4 @@ class TestIngress:
         assert rule_0["host"] == "deployments.example.com"
 
         assert ingressAnnotation == docs[1]["metadata"]["annotations"]
+        assert docs[1]["spec"]["tls"][0]["secretName"] == "astronomer-tls"
