2a4b1d316874ff6515e88a432e4fa8ff95c675bd
Daniel Hoherd <danielh@astronomer.io>
2024-12-04 09:59:19 -0500

    Dev updates 2024-12-03 (#550)
    * Update to latest kind versions
    * Rebuild python test dependencies
    * Use new circleci machine image version
    * Bump pre-commit hook versions, remove black hook
    * Expand ruff configs and apply ruff-format
    * Add PR description GHA workflow that we recently added to astronomer/astronomer
    * Setup venv dir using uv if possible (we already do this in astronomer/astronomer)

--- tests/chart/test_extra_objects.py
+++ tests/chart/test_extra_objects.py
@@ -3,9 +3,8 @@ from subprocess import CalledProcessError
 
 import pytest
 
+from tests import supported_k8s_versions
 from tests.chart.helm_template_generator import render_chart
-
-from .. import supported_k8s_versions
 
 
 @pytest.mark.parametrize("kube_version", supported_k8s_versions)
