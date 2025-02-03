1a4396556078510dd1e8051f43b27386949b19fb
pgvishnuram <81585115+pgvishnuram@users.noreply.github.com>
2025-02-03 15:04:46 +0530

    add git sync relay service account support (#561)
    * add git sync relay service account support
    
    * update default values
    
    * fix pre-commit
    
    * add service account support in git sync deployment
    
    * add git sync relay tests

---
+++ tests/chart/test_git_sync_relay_serviceaccount.py
@@ -0,0 +1,93 @@
+import pytest
+
+from tests import supported_k8s_versions
+from tests.chart.helm_template_generator import render_chart
+
+
+@pytest.mark.parametrize("kube_version", supported_k8s_versions)
+class TestGitSyncRelayServiceAccount:
+    def test_git_sync_relay_service_default(self, kube_version):
+        """Test that no git-sync-relay service templates are rendered by default."""
+        docs = render_chart(
+            kube_version=kube_version,
+            show_only="templates/git-sync-relay/git-sync-relay-serviceaccount.yaml",
+        )
+        assert len(docs) == 0
+
+    def test_git_sync_relay_service_enabled(self, kube_version):
+        """Test that a valid serviceAccount is rendered when git-sync-relay is enabled."""
+        values = {"gitSyncRelay": {"enabled": True}}
+        docs = render_chart(
+            kube_version=kube_version,
+            show_only="templates/git-sync-relay/git-sync-relay-serviceaccount.yaml",
+            values=values,
+        )
+        assert len(docs) == 1
+        doc = docs[0]
+        assert doc["kind"] == "ServiceAccount"
+        assert doc["apiVersion"] == "v1"
+        assert doc["metadata"]["name"] == "release-name-git-sync-relay"
+
+    def test_git_sync_relay_server_annotations(self, kube_version):
+        """Test that a valid serviceAccount is rendered with proper annotations
+        when git_sync_relay is enabled and annotations are provided."""
+        annotations = {"foo-key": "foo-value", "bar-key": "bar-value"}
+        values = {
+            "gitSyncRelay": {
+                "enabled": True,
+                "serviceAccount": {"annotations": annotations},
+            }
+        }
+        docs = render_chart(
+            kube_version=kube_version,
+            show_only=[
+                "templates/git-sync-relay/git-sync-relay-serviceaccount.yaml",
+                "templates/git-sync-relay/git-sync-relay-deployment.yaml",
+            ],
+            values=values,
+        )
+        assert len(docs) == 2
+        doc = docs[0]
+        assert doc["kind"] == "ServiceAccount"
+        assert doc["apiVersion"] == "v1"
+        assert doc["metadata"]["name"] == "release-name-git-sync-relay"
+        assert doc["metadata"]["annotations"] == annotations
+        assert "release-name-git-sync-relay" == docs[1]["spec"]["template"]["spec"]["serviceAccountName"]
+
+    def test_git_sync_relay_server_serviceaccount_overrides_defaults(self, kube_version):
+        """Test that a serviceAccount overridable with disabled creation"""
+        values = {
+            "gitSyncRelay": {
+                "enabled": True,
+                "serviceAccount": {"create": False},
+            }
+        }
+        docs = render_chart(
+            kube_version=kube_version,
+            show_only=[
+                "templates/git-sync-relay/git-sync-relay-serviceaccount.yaml",
+                "templates/git-sync-relay/git-sync-relay-deployment.yaml",
+            ],
+            values=values,
+        )
+        assert len(docs) == 1
+        assert "default" == docs[0]["spec"]["template"]["spec"]["serviceAccountName"]
+
+    def test_git_sync_relay_server_serviceaccount_overrides(self, kube_version):
+        """Test that a serviceAccount overridable with disabled creation"""
+        values = {
+            "gitSyncRelay": {
+                "enabled": True,
+                "serviceAccount": {"create": False, "name": "git-sync-relay"},
+            }
+        }
+        docs = render_chart(
+            kube_version=kube_version,
+            show_only=[
+                "templates/git-sync-relay/git-sync-relay-serviceaccount.yaml",
+                "templates/git-sync-relay/git-sync-relay-deployment.yaml",
+            ],
+            values=values,
+        )
+        assert len(docs) == 1
+        assert "git-sync-relay" == docs[0]["spec"]["template"]["spec"]["serviceAccountName"]
