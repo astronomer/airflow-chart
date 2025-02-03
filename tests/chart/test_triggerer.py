1a91bab8c9ba5e120a7d14f62bb69a0bef1dba99
pgvishnuram <81585115+pgvishnuram@users.noreply.github.com>
2025-02-03 15:11:54 +0530

    update airflow chart backend version 1.13.2-astro (#560)
    * update airflow chart backend version 1.13.2-astro
    
    * update chart hash and chart lock
    
    * fix pre-commit
    
    * update log groomer sidecar tests
    
    * update scheduler with log groomer tests

--- tests/chart/test_triggerer.py
+++ tests/chart/test_triggerer.py
@@ -1,9 +1,15 @@
 from tests.chart.helm_template_generator import render_chart
 
+from . import get_containers_by_name
+
 
 class TestTriggerer:
     def test_triggerer_log_grommer_defaults(self):
         """Test Triggerer  Log Groomer defaults."""
+        default_env_vars = [
+            {"name": "AIRFLOW__LOG_RETENTION_DAYS", "value": "15"},
+            {"name": "AIRFLOW_HOME", "value": "/usr/local/airflow"},
+        ]
         docs = render_chart(
             values={
                 "airflow": {"airflowVersion": "2.4.3"},
@@ -11,4 +17,24 @@ class TestTriggerer:
             show_only=["charts/airflow/templates/triggerer/triggerer-deployment.yaml"],
         )
         assert len(docs) == 1
-        assert "/usr/local/bin/clean-airflow-logs" in docs[0]["spec"]["template"]["spec"]["containers"][1]["args"]
+        c_by_name = get_containers_by_name(docs[0])
+        assert "/usr/local/bin/clean-airflow-logs" in c_by_name["triggerer-log-groomer"]["args"]
+        print(c_by_name["triggerer-log-groomer"]["env"])
+        assert default_env_vars == c_by_name["triggerer-log-groomer"]["env"]
+
+    def test_triggerer_log_grommer_overrides(self):
+        """Test Triggerer  Log Groomer with custom env vars."""
+        env = {"name": "ASTRONOMER__AIRFLOW___LOG_RETENTION_DAYS", "value": "5"}
+        docs = render_chart(
+            values={
+                "airflow": {
+                    "airflowVersion": "2.4.3",
+                    "triggerer": {"logGroomerSidecar": {"env": [env]}},
+                },
+            },
+            show_only=["charts/airflow/templates/triggerer/triggerer-deployment.yaml"],
+        )
+        assert len(docs) == 1
+        c_by_name = get_containers_by_name(docs[0])
+        assert "/usr/local/bin/clean-airflow-logs" in c_by_name["triggerer-log-groomer"]["args"]
+        assert env in c_by_name["triggerer-log-groomer"]["env"]
