0508b5ba0a0517e3870a42a981bb24a5c4a9c1fc
Daniel Hoherd <daniel.hoherd@gmail.com>
2025-03-01 09:50:50 -0500

    pre-commit run --all-files

--- tests/functional/test_chart.py
+++ tests/functional/test_chart.py
@@ -80,9 +80,9 @@ def test_astronomer_airflow_check_version(webserver):
         print("astronomer-airflow-version-check pip module is not installed")
         return
     version = version_check_module["version"]
-    assert semantic_version(version) >= semantic_version(
-        "1.0.1"
-    ), "astronomer-airflow-version-check module must be greater than 1.0.0"
+    assert semantic_version(version) >= semantic_version("1.0.1"), (
+        "astronomer-airflow-version-check module must be greater than 1.0.0"
+    )
 
 
 def test_airflow_connections(scheduler):
