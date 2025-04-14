c2aa68d699c9fb9533f6f55472bbc270ccdb3ed0
pgvishnuram <vishnu@astronomer.io>
2025-04-14 18:22:35 +0530

    add dag processor test cases

---
+++ tests/chart/test_dag_processor.py
@@ -0,0 +1,51 @@
+from tests.chart.helm_template_generator import render_chart
+
+from . import get_containers_by_name
+
+
+class TestDagProcessor:
+    def test_dag_processor_defaults(self):
+        """Test Dag Processor defaults."""
+        docs = render_chart(
+            values={},
+            show_only=[
+                "charts/airflow/templates/dag-processor/dag-processor-deployment.yaml",
+                "charts/airflow/templates/dag-processor/dag-processor-serviceaccount.yaml",
+            ],
+        )
+        assert len(docs) == 0
+
+    def test_dag_processor_log_grommer_defaults(self):
+        """Test Dag Processor enabled defaults with log groomer."""
+        default_env_vars = [
+            {"name": "AIRFLOW__LOG_RETENTION_DAYS", "value": "15"},
+            {"name": "AIRFLOW_HOME", "value": "/usr/local/airflow"},
+        ]
+        docs = render_chart(
+            values={
+                "airflow": {"airflowVersion": "2.4.3", "dagProcessor": {"enabled": True}},
+            },
+            show_only=["charts/airflow/templates/dag-processor/dag-processor-deployment.yaml"],
+        )
+        assert len(docs) == 1
+        c_by_name = get_containers_by_name(docs[0])
+        assert "/usr/local/bin/clean-airflow-logs" in c_by_name["dag-processor-log-groomer"]["args"]
+        print(c_by_name["dag-processor-log-groomer"]["env"])
+        assert default_env_vars == c_by_name["dag-processor-log-groomer"]["env"]
+
+    def test_triggerer_log_grommer_overrides(self):
+        """Test Dag Processor enabled defaults with log groomer custom env vars."""
+        env = {"name": "ASTRONOMER__AIRFLOW___LOG_RETENTION_DAYS", "value": "5"}
+        docs = render_chart(
+            values={
+                "airflow": {
+                    "airflowVersion": "2.4.3",
+                    "dagProcessor": {"enabled": True, "logGroomerSidecar": {"env": [env]}},
+                },
+            },
+            show_only=["charts/airflow/templates/dag-processor/dag-processor-deployment.yaml"],
+        )
+        assert len(docs) == 1
+        c_by_name = get_containers_by_name(docs[0])
+        assert "/usr/local/bin/clean-airflow-logs" in c_by_name["dag-processor-log-groomer"]["args"]
+        assert env in c_by_name["dag-processor-log-groomer"]["env"]
