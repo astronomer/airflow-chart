ab776aad917e1f3f9a2ad9993a3c08280f2fbd2a
pgvishnuram <vishnu@astronomer.io>
2025-04-14 16:12:27 +0530

    update scc tests

--- tests/chart/test_scc.py
+++ tests/chart/test_scc.py
@@ -19,9 +19,24 @@ class TestAirflowSccPrivileges:
         """Test airflow scc privileges with defaults - disabled."""
         docs = render_chart(
             kube_version=kube_version,
-            values={
-                "sccEnabled": True
-            },
+            values={"sccEnabled": True},
             show_only="templates/airflow-scc-anyuid.yaml",
         )
         assert len(docs) == 1
+        assert docs[0]["kind"] == "SecurityContextConstraints"
+        assert docs[0]["apiVersion"] == "security.openshift.io/v1"
+        assert docs[0]["metadata"]["name"] == "release-name-anyuid"
+        assert docs[0]["users"] == [
+            "system:serviceaccount:default:default",
+            "system:serviceaccount:default:release-name-airflow-webserver",
+            "system:serviceaccount:default:release-name-airflow-redis",
+            "system:serviceaccount:default:release-name-airflow-flower",
+            "system:serviceaccount:default:release-name-airflow-scheduler",
+            "system:serviceaccount:default:release-name-airflow-statsd",
+            "system:serviceaccount:default:release-name-airflow-create-user-job",
+            "system:serviceaccount:default:release-name-airflow-migrate-database-job",
+            "system:serviceaccount:default:release-name-airflow-worker",
+            "system:serviceaccount:default:release-name-airflow-triggerer",
+            "system:serviceaccount:default:release-name-airflow-pgbouncer",
+            "system:serviceaccount:default:release-name-airflow-cleanup",
+        ]
