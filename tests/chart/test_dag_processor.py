170fa68e7683a6be8baedcffa4ddb3423bc43201
Shubham-astro <shubham.singh@astronomer.io>
2025-07-16 15:37:31 +0530

    test-fix

--- tests/chart/test_dag_processor.py
+++ tests/chart/test_dag_processor.py
@@ -19,8 +19,8 @@ class TestDagProcessor:
         """Test Dag Processor enabled defaults with log groomer."""
         default_env_vars = [
             {"name": "AIRFLOW__LOG_RETENTION_DAYS", "value": "15"},
+            {"name": "AIRFLOW__LOG_CLEANUP_FREQUENCY_MINUTES", "value": "15"},
             {"name": "AIRFLOW_HOME", "value": "/usr/local/airflow"},
-            {'name': 'AIRFLOW__LOG_CLEANUP_FREQUENCY_MINUTES', 'value': '15'},
         ]
         docs = render_chart(
             values={
