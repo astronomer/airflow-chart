78d8e71a0d4966ec60c356c8da5f20fd25b77d7a
Shubham-astro <shubham.singh@astronomer.io>
2025-11-10 11:57:44 +0530

    Adding readOnlyRootFilesystem security context to pgbouncer SSL certificate (#662)
    * add securitycontext
    
    * add test
    
    * add temp directory
    
    * precommit

--- tests/chart/test_pgbouncer_certgenerator.py
+++ tests/chart/test_pgbouncer_certgenerator.py
@@ -35,6 +35,7 @@ class TestPgbouncersslFeature:
         assert expected_rbac in docs[1]["rules"]
         assert "RoleBinding" == jmespath.search("kind", docs[2])
         assert docs[3]["spec"]["template"]["spec"]["affinity"] == {}
+        assert docs[3]["spec"]["template"]["spec"]["containers"][0]["securityContext"]["readOnlyRootFilesystem"] is True
 
     def test_pgbouncer_certgenerator_with_custom_registry_secret(self, kube_version):
         """Test pgbouncer certgenerator sslmode opts result."""
@@ -50,6 +51,7 @@ class TestPgbouncersslFeature:
         )
         assert len(docs) == 4
         assert [{"name": "gscsecret"}] == docs[3]["spec"]["template"]["spec"]["imagePullSecrets"]
+        assert docs[3]["spec"]["template"]["spec"]["containers"][0]["securityContext"]["readOnlyRootFilesystem"] is True
 
     def test_pgbouncer_certgenerator_pgbouncerssl_extraannotations(self, kube_version):
         """Test that certgenerator.extraAnnotations correctly inserts the annotations."""
@@ -71,6 +73,7 @@ class TestPgbouncersslFeature:
         )
         assert len(docs) == 4
         assert docs[3]["spec"]["template"]["metadata"]["annotations"] == extraAnnotations
+        assert docs[3]["spec"]["template"]["spec"]["containers"][0]["securityContext"]["readOnlyRootFilesystem"] is True
 
     def test_pgbouncer_certgenerator_pgbouncerssl_affinity(self, kube_version):
         """Test that certgenerator.affinity correctly inserts the affinity."""
@@ -108,6 +111,7 @@ class TestPgbouncersslFeature:
         )
         assert len(docs) == 4
         assert docs[3]["spec"]["template"]["spec"]["affinity"] == affinity
+        assert docs[3]["spec"]["template"]["spec"]["containers"][0]["securityContext"]["readOnlyRootFilesystem"] is True
 
     def test_pgbouncer_certgenerator_pgbouncerssl_extra_labels_support(self, kube_version):
         """Test that certgenerator has proper custom labels."""
